cffaa0e0fbdd4583c337b6751d8ebac9057a6d35
Michael Salerno <michael.j.salerno@stonybrook.edu>
2016-11-18 08:14:36 -0500

    added args

--- info_gen.py
+++ info_gen.py
@@ -1,26 +1,60 @@
+#!/usr/bin/env python
+import argparse
 import json
 
-album = 'Bob Dylan: The 1966 Live Recordings'
-artist = 'Bob Dylan'
-disc_total = 36
 
-with open('tracks.txt') as tracks:
-    track_lst = tracks.readlines()
-track_index = 0
-disc = 1
+def main():
+    parser = argparse.ArgumentParser(description='Generate info JSON for the on_track program.')
 
-with open('comments.txt') as comments:
-    for com in comments:
-        info = {'album': album, 'comment': com.strip(), 'artist': artist, 'disc': disc, 'disc_total': disc_total, 'tracks': []}
+    parser.add_argument('--artist', help='Specify the name of the artist.', type=str, required=True, dest='art',
+                        metavar='<artist>')
 
-        for i in range(track_index, len(track_lst)):
-            track = track_lst[i].strip()
+    parser.add_argument('--album', help='Specify the name of the album.', type=str, required=True, dest='alb',
+                        metavar='<album>')
 
-            if len(track) == 0:
-                with open('result.json', 'w') as fp:
-                    json.dump(info, fp, indent=4)
-                track_index = i + 1
-                disc += 1
-                break
-            else:
-                info['tracks'].append(track)
+    parser.add_argument('--disc-total', '-dt', help='Specify the number of discs.', type=int, required=True, dest='dt',
+                        metavar='<disc total>')
+
+    parser.add_argument('--tracks', '-t', help='Specify the tracks file.', type=str, required=True, dest='tracks',
+                        metavar='<tracks file>')
+
+    parser.add_argument('--comments', '-c', help='Specify the comments file.', type=str, required=True, dest='comm',
+                        metavar='<comments file>')
+
+    parser.add_argument('--output-dir', '-o', help='Specify the output directory.', type=str, required=False,
+                        dest='dir', metavar='<comments file>', default='./')
+
+    args = parser.parse_args()
+
+    if not args.dir.endswith(('\\', '/')):
+        args.dir += '/'
+
+    album = args.alb  # 'Bob Dylan: The 1966 Live Recordings'
+    artist = args.art  # 'Bob Dylan'
+    disc_total = args.dt  # 36
+
+    with open(args.tracks) as tracks:
+        track_lst = tracks.readlines()
+    track_index = 0
+    disc = 1
+
+    with open(args.comm) as comments:
+        for com in comments:
+            info = {'album': album, 'comment': com.strip(), 'artist': artist, 'disc': disc, 'disc_total': disc_total,
+                    'tracks': []}
+
+            for i in range(track_index, len(track_lst)):
+                track = track_lst[i].strip()
+
+                if len(track) == 0:
+                    with open(args.dir + 'info-' + str(disc).zfill(2) + '.json', 'w') as fp:
+                        json.dump(info, fp, indent=4)
+                    track_index = i + 1
+                    disc += 1
+                    break
+                else:
+                    info['tracks'].append(track)
+
+
+if __name__ == "__main__":
+    main()
--- on_track.py
+++ on_track.py
@@ -1,45 +1,77 @@
+import argparse
 import glob
 import json
 import os
 import sys
 import mutagen
 
-info = None
-files = glob.glob('*.json')
-if len(files) != 1:
-    print('Please have 1 json file in each folder.')
-    sys.exit(1)
 
-with open(files[0]) as data_file:
-    info = json.load(data_file)
+def main():
+    parser = argparse.ArgumentParser(description='Generate info JSON for the on_track program.')
 
-files = glob.glob('*.flac')
-if len(files) != len(info['tracks']):
-    print('the number of tracks given in the json is different than the number of files.')
-    print('files: ' + str(len(files)) + ' tracks: ' + str(len(info['tracks'])))
-    sys.exit(1)
+    parser.add_argument('--quiet', '-q', help='Only print out errors.', required=False, dest='q', action='store_true')
+    parser.add_argument('--recursive', '-r', help='Go through all sub folders.', required=False, dest='recur', action='store_true')
+    parser.add_argument('--extension', '-ex', help='Specify the file extensions of the music files.', type=str, required=False, dest='ex',
+                            metavar='<extension>', default='flac')
+    args = parser.parse_args()
 
-for f in files:
-    num = int(f.split()[0]) - 1
-    print('fixing: ' + f)
-    print('     album:       ' + info['album'])
-    print('     artist:      ' + info['artist'])
-    print('     disc:        ' + str(info['disc']))
-    print('     total discs: ' + str(info['disc_total']))
-    print('     comment:     ' + str(info['comment']))
-    print('     title:       ' + str(info['tracks'][num]) + '\n')
+    if not args.recur:
+        fix_tracks(args.q, args.ex)
+    else:
+        start = os.getcwd()
+        for root, dirs, files in os.walk('.'):
+            if root == '.':
+                continue
+            print('chdir: ' + root)
+            os.chdir(root)
+            fix_tracks(args.q, args.ex)
+            os.chdir(start)
 
-    audio = mutagen.File(f)
-    for name in audio:
-        pass
-        #print name + ' : ' + str(audio[name])
 
-    audio['album'] = info['album']
-    audio['artist'] = info['artist']
-    audio['discnumber'] = str(info['disc'])
-    audio['disctotal'] = str(info['disc_total'])
-    audio['comment'] = info['comment']
-    audio['title'] = info['tracks'][num]
-    audio.save()
+def fix_tracks(q, ex):
+    info = None
+    files = glob.glob('*.json')
+    if len(files) != 1:
+        print('Please have 1 json file in each folder: ' + os.getcwd())
+        sys.exit(1)
 
-    os.rename(f, str(num+1).zfill(2) + ' ' + info['tracks'][num] + '.flac')
+    with open(files[0]) as data_file:
+        info = json.load(data_file)
+
+    files = glob.glob('*.' + ex)
+    if len(files) != len(info['tracks']):
+        print('the number of tracks given in the json is different than the number of files.')
+        print('files: ' + str(len(files)) + ' tracks: ' + str(len(info['tracks'])))
+        print('dir: ' + os.getcwd())
+        sys.exit(1)
+
+    for f in files:
+        num = int(f.split()[0]) - 1
+        new_name = str(num+1).zfill(2) + ' ' + info['tracks'][num] + '.' + ex
+        new_name = new_name.replace('/', '_')
+        new_name = new_name.replace('\\', '_')
+        if not q:
+            print('fixing: ' + f)
+            print('     album:       ' + info['album'])
+            print('     artist:      ' + info['artist'])
+            print('     disc:        ' + str(info['disc']))
+            print('     total discs: ' + str(info['disc_total']))
+            print('     comment:     ' + str(info['comment']))
+            print('     title:       ' + str(info['tracks'][num]))
+            print('     rename:      ' + new_name + '\n')
+
+        audio = mutagen.File(f)
+
+        audio['album'] = info['album']
+        audio['artist'] = info['artist']
+        audio['discnumber'] = str(info['disc'])
+        audio['disctotal'] = str(info['disc_total'])
+        audio['comment'] = info['comment']
+        audio['title'] = info['tracks'][num]
+        audio.save()
+
+        os.rename(f, new_name)
+
+
+if __name__ == "__main__":
+    main()
